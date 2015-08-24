9e9e1c844fccef6efdd61304c934853940851cc8
Gauthier Cornu <cornu.gauthier@gmail.com>
2015-08-24 22:21:56 +0200

    User interface test

--- Connect.vb
+++ Connect.vb
@@ -1,31 +1,24 @@
 ﻿Imports gcornu.Interop
 Imports gcornu.Interop.Extensibility
-Imports Microsoft.Office.Core
 Imports System.Windows.Forms
 Imports System.Runtime.InteropServices
+Imports Microsoft.Office.Core
+Imports System.Drawing
 
 <ComVisible(True), Guid("01234567-89ab-cdef-0123-456789abcdef"), ProgId("MyVBAAddin.Connect")> _
 Public Class Connect
-    Implements Extensibility.IDTExtensibility2
+    Implements IDTExtensibility2
 
     Private _VBE As VBAExtensibility.VBE
     Private _AddIn As VBAExtensibility.AddIn
+    Private WithEvents _CommandBarButton1 As CommandBarButton
+    Private WithEvents _CommandBarButton2 As CommandBarButton
 
-    ' Buttons created by the add-in
-    Private WithEvents _myStandardCommandBarButton As CommandBarButton
-    Private WithEvents _myToolsCommandBarButton As CommandBarButton
-    Private WithEvents _myCodeWindowCommandBarButton As CommandBarButton
-    Private WithEvents _myToolBarButton As CommandBarButton
-    Private WithEvents _myCommandBarPopup1Button As CommandBarButton
-    Private WithEvents _myCommandBarPopup2Button As CommandBarButton
+    Private _toolWindow1 As VBAExtensibility.Window
+    Private _toolWindow2 As VBAExtensibility.Window
 
-    ' CommandBars created by the add-in
-    Private _myToolbar As CommandBar
-    Private _myCommandBarPopup1 As CommandBarPopup
-    Private _myCommandBarPopup2 As CommandBarPopup
-
-    Private Sub OnConnection(Application As Object, ConnectMode As Extensibility.ext_ConnectMode, _
-       AddInInst As Object, ByRef custom As System.Array) Implements IDTExtensibility2.OnConnection
+    Private Sub OnConnection(Application As Object, ConnectMode As ext_ConnectMode, AddInInst As Object, _
+       ByRef custom As System.Array) Implements IDTExtensibility2.OnConnection
 
         Try
 
@@ -34,10 +27,10 @@ Public Class Connect
 
             Select Case ConnectMode
 
-                Case Extensibility.ext_ConnectMode.ext_cm_Startup
+                Case ext_ConnectMode.ext_cm_Startup
                     ' OnStartupComplete will be called
 
-                Case Extensibility.ext_ConnectMode.ext_cm_AfterStartup
+                Case ext_ConnectMode.ext_cm_AfterStartup
                     InitializeAddIn()
 
             End Select
@@ -50,51 +43,23 @@ Public Class Connect
 
     End Sub
 
-    Private Sub OnDisconnection(RemoveMode As Extensibility.ext_DisconnectMode, _
+    Private Sub OnDisconnection(RemoveMode As ext_DisconnectMode, _
        ByRef custom As System.Array) Implements IDTExtensibility2.OnDisconnection
 
-        Try
+        If Not _CommandBarButton1 Is Nothing Then
 
-            Select Case RemoveMode
+            _CommandBarButton1.Delete()
+            _CommandBarButton1 = Nothing
 
-                Case ext_DisconnectMode.ext_dm_HostShutdown, ext_DisconnectMode.ext_dm_UserClosed
+        End If
 
-                    ' Delete buttons on built-in commandbars
-                    If Not (_myStandardCommandBarButton Is Nothing) Then
-                        _myStandardCommandBarButton.Delete()
-                    End If
+        If Not _CommandBarButton2 Is Nothing Then
 
-                    If Not (_myCodeWindowCommandBarButton Is Nothing) Then
-                        _myCodeWindowCommandBarButton.Delete()
-                    End If
+            _CommandBarButton2.Delete()
+            _CommandBarButton2 = Nothing
 
-                    If Not (_myToolsCommandBarButton Is Nothing) Then
-                        _myToolsCommandBarButton.Delete()
-                    End If
+        End If
 
-                    ' Disconnect event handlers
-                    _myToolBarButton = Nothing
-                    _myCommandBarPopup1Button = Nothing
-                    _myCommandBarPopup2Button = Nothing
-
-                    ' Delete commandbars created by the add-in
-                    If Not (_myToolbar Is Nothing) Then
-                        _myToolbar.Delete()
-                    End If
-
-                    If Not (_myCommandBarPopup1 Is Nothing) Then
-                        _myCommandBarPopup1.Delete()
-                    End If
-
-                    If Not (_myCommandBarPopup2 Is Nothing) Then
-                        _myCommandBarPopup2.Delete()
-                    End If
-
-            End Select
-
-        Catch e As System.Exception
-            System.Windows.Forms.MessageBox.Show(e.ToString)
-        End Try
     End Sub
 
     Private Sub OnStartupComplete(ByRef custom As System.Array) _
@@ -109,124 +74,120 @@ Public Class Connect
 
     End Sub
 
-    Private Sub OnBeginShutdown(ByRef custom As System.Array) _
-       Implements IDTExtensibility2.OnBeginShutdown
+    Private Sub OnBeginShutdown(ByRef custom As System.Array) Implements IDTExtensibility2.OnBeginShutdown
 
     End Sub
 
-    Private Function AddCommandBarButton(ByVal commandBar As CommandBar, caption As String) As CommandBarButton
-
-        Dim commandBarButton As CommandBarButton
-        Dim commandBarControl As CommandBarControl
-
-        commandBarControl = commandBar.Controls.Add(MsoControlType.msoControlButton)
-        commandBarButton = DirectCast(commandBarControl, CommandBarButton)
-
-        commandBarButton.Caption = caption
-        commandBarButton.FaceId = 59
-
-        Return commandBarButton
-
-    End Function
-
     Private Sub InitializeAddIn()
 
-        ' Constants for names of built-in commandbars of the VBA editor
-        Const TOOLS_COMMANDBAR_NAME As String = "Tools"
-
-        ' Constants for names of commandbars created by the add-in
-        Const MY_COMMANDBAR_POPUP1_NAME As String = "TemporaryCommandBarPopup1"
-
-        ' Constants for captions of commandbars created by the add-in
-        Const MY_COMMANDBAR_POPUP1_CAPTION As String = "Git For VBA"
-        Const MY_TOOLBAR_CAPTION As String = "Git For VBA toolbar"
-
-        ' Built-in commandbars of the VBA editor
-        Dim toolsCommandBar As CommandBar
-
-        ' Other variables
-        Dim toolsCommandBarControl As CommandBarControl
-        Dim position As Integer
+        Dim standardCommandBar As CommandBar
+        Dim commandBarControl As CommandBarControl
 
         Try
 
-            ' Retrieve some built-in commandbars
-            toolsCommandBar = _VBE.CommandBars.Item(TOOLS_COMMANDBAR_NAME)
+            standardCommandBar = _VBE.CommandBars.Item("Standard")
 
-            ' ------------------------------------------------------------------------------------
-            ' New toolbar
-            ' ------------------------------------------------------------------------------------
+            commandBarControl = standardCommandBar.Controls.Add(MsoControlType.msoControlButton)
+            _CommandBarButton1 = DirectCast(commandBarControl, CommandBarButton)
+            _CommandBarButton1.Caption = "Toolwindow 1"
+            _CommandBarButton1.FaceId = 59
+            _CommandBarButton1.Style = MsoButtonStyle.msoButtonIconAndCaption
+            _CommandBarButton1.BeginGroup = True
 
-            ' Add a new toolbar 
-            _myToolbar = _VBE.CommandBars.Add(MY_TOOLBAR_CAPTION, MsoBarPosition.msoBarTop, System.Type.Missing, True)
+            commandBarControl = standardCommandBar.Controls.Add(MsoControlType.msoControlButton)
+            _CommandBarButton2 = DirectCast(commandBarControl, CommandBarButton)
+            _CommandBarButton2.Caption = "Toolwindow 2"
+            _CommandBarButton2.FaceId = 59
+            _CommandBarButton2.Style = MsoButtonStyle.msoButtonIconAndCaption
+            _CommandBarButton2.BeginGroup = True
 
-            ' Add a new button on that toolbar
-            _myToolBarButton = AddCommandBarButton(_myToolbar, "Button1")
+        Catch ex As Exception
 
-            ' Make visible the toolbar
-            _myToolbar.Visible = True
+            MessageBox.Show(ex.ToString())
 
-            ' ------------------------------------------------------------------------------------
-            ' New submenu under the "Tools" menu
-            ' ------------------------------------------------------------------------------------
-
-            ' Add a new commandbar popup 
-            _myCommandBarPopup1 = DirectCast(toolsCommandBar.Controls.Add( _
-               MsoControlType.msoControlPopup, System.Type.Missing, System.Type.Missing, _
-               toolsCommandBar.Controls.Count + 1, True), CommandBarPopup)
-
-            ' Change some commandbar popup properties
-            _myCommandBarPopup1.CommandBar.Name = MY_COMMANDBAR_POPUP1_NAME
-            _myCommandBarPopup1.Caption = MY_COMMANDBAR_POPUP1_CAPTION
-
-            ' Add a new button on that commandbar popup
-            _myCommandBarPopup1Button = AddCommandBarButton(_myCommandBarPopup1.CommandBar, "Button2")
-
-            ' Make visible the commandbar popup
-            _myCommandBarPopup1.Visible = True
-
-            ' Calculate the position of a new commandbar popup to the right of the "Tools" menu
-            toolsCommandBarControl = DirectCast(toolsCommandBar.Parent, CommandBarControl)
-            position = toolsCommandBarControl.Index + 1
-
-        Catch e As System.Exception
-            System.Windows.Forms.MessageBox.Show(e.ToString)
         End Try
 
     End Sub
 
-    Private Sub _myToolBarButton_Click(Ctrl As Microsoft.Office.Core.CommandBarButton, _
-       ByRef CancelDefault As Boolean) Handles _myToolBarButton.Click
+    Private Function CreateToolWindow(ByVal toolWindowCaption As String, ByVal toolWindowGuid As String, _
+       ByVal toolWindowUserControl As UserControl) As VBAExtensibility.Window
 
-        MessageBox.Show("Clicked " & Ctrl.Caption)
+        Dim userControlObject As Object = Nothing
+        Dim userControlHost As UserControlHost
+        Dim toolWindow As VBAExtensibility.Window
+        Dim progId As String
+
+        ' IMPORTANT: ensure that you use the same ProgId value used in the ProgId attribute of the UserControlHost class
+        progId = "MyVBAAddin.UserControlHost"
+
+        toolWindow = _VBE.Windows.CreateToolWindow(_AddIn, progId, toolWindowCaption, toolWindowGuid, userControlObject)
+        userControlHost = DirectCast(userControlObject, UserControlHost)
+
+        toolWindow.Visible = True
+
+        userControlHost.AddUserControl(toolWindowUserControl)
+
+        Return toolWindow
+
+    End Function
+
+    Private Sub _CommandBarButton1_Click(Ctrl As Microsoft.Office.Core.CommandBarButton, _
+       ByRef CancelDefault As Boolean) Handles _CommandBarButton1.Click
+
+        Dim userControlObject As Object = Nothing
+        Dim userControlToolWindow1 As UserControlToolWindow1
+
+        Try
+
+            If _toolWindow1 Is Nothing Then
+
+                userControlToolWindow1 = New UserControlToolWindow1()
+
+                _toolWindow1 = CreateToolWindow("My toolwindow 1", "21234567-89ab-cdef-0123-456789abcdef", userControlToolWindow1)
+
+                userControlToolWindow1.Initialize(_VBE)
+
+            Else
+
+                _toolWindow1.Visible = True
+
+            End If
+
+        Catch ex As Exception
+
+            MessageBox.Show(ex.ToString)
+
+        End Try
 
     End Sub
 
-    Private Sub _myToolsCommandBarButton_Click(Ctrl As Microsoft.Office.Core.CommandBarButton, _
-       ByRef CancelDefault As Boolean) Handles _myToolsCommandBarButton.Click
+    Private Sub _CommandBarButton2_Click(Ctrl As Microsoft.Office.Core.CommandBarButton, _
+       ByRef CancelDefault As Boolean) Handles _CommandBarButton2.Click
 
-        MessageBox.Show("Clicked " & Ctrl.Caption)
+        Dim userControlObject As Object = Nothing
+        Dim userControlToolWindow2 As UserControlToolWindow2
 
-    End Sub
+        Try
 
-    Private Sub _myStandardCommandBarButton_Click(Ctrl As Microsoft.Office.Core.CommandBarButton, _
-       ByRef CancelDefault As Boolean) Handles _myStandardCommandBarButton.Click
+            If _toolWindow2 Is Nothing Then
 
-        MessageBox.Show("Clicked " & Ctrl.Caption)
+                userControlToolWindow2 = New UserControlToolWindow2()
 
-    End Sub
+                _toolWindow2 = CreateToolWindow("My toolwindow 2", "31234567-89ab-cdef-0123-456789abcdef", userControlToolWindow2)
 
-    Private Sub _myCodeWindowCommandBarButton_Click(Ctrl As Microsoft.Office.Core.CommandBarButton, _
-       ByRef CancelDefault As Boolean) Handles _myCodeWindowCommandBarButton.Click
+                userControlToolWindow2.Initialize(_VBE)
 
-        MessageBox.Show("Clicked " & Ctrl.Caption)
+            Else
 
-    End Sub
+                _toolWindow2.Visible = True
 
-    Private Sub _myCommandBarPopup1Button_Click(Ctrl As Microsoft.Office.Core.CommandBarButton, _
-       ByRef CancelDefault As Boolean) Handles _myCommandBarPopup1Button.Click
+            End If
 
-        MessageBox.Show("Clicked " & Ctrl.Caption)
+        Catch ex As Exception
+
+            MessageBox.Show(ex.ToString)
+
+        End Try
 
     End Sub
 
